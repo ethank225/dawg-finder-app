733d7b5bfeae6ce5df2b0a4cecb38f25703124a9
ethank225 <dendenk3@gmail.com>
2025-03-01 10:28:06 -0800

    Fixed date-fns dependency issue

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,7 +3,7 @@ import Database from "better-sqlite3";
 
 // Connect to SQLite database
 const path = require("path");
-const dbPath = "/Users/ethan/Desktop/dawgfinder/backend/data/all_data.db";
+const dbPath = "./backend/data/all_data.db";
 const db = new Database(dbPath, { fileMustExist: true });
 
 export async function GET(req) {
